3f159880344ac49ef163c6d393165dbe7bacdc94
Furkan Öznam <furkan.oznam@yandex.com.tr>
2021-01-24 14:22:25 +0300

    Add project files.

---
+++ 001.Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _001.Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Ogrenci = new Dictionary<int, string>();
+            Ogrenci.Add(101, "Furkan");
+            Ogrenci.Add(338, "Mehmet");
+            Ogrenci.Add(776, "Lila");
+            Ogrenci.Add(6067, "Ayla");
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _002.NervousTastyCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Burada new dediğimiz anda, class içerisindeki constructor çalışır.
+            Customer customer1 = new Customer { Id = 1, FirstName = "Furkan", LastName = "Öznam", City = "İstanbul" };
+            Customer customer2 = new Customer(2, "Efna", "Öznam", "Trabzon");
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Mehmet";
+            customer3.LastName = "Akıncı";
+            customer3.City = "Konya";
+        }
+    }
+
+    class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+            Console.WriteLine("Yapıcı blok çalıştı.");
+        }
+
+        public Customer(int id, string firstname, string lastname, string city)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
